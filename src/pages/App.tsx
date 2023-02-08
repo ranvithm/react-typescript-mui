5ed95f79897db6434e06fd2595ec4790294124f4
Ranjithkumar7 <ranjithkumarm@gain-insights.com>
2023-02-08 13:55:09 +0530

    Folder structure and enable eslint

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import { Container, Box } from "@mui/material"
+
+const App = () => {
+    return (
+        <Container>
+            <Box display="flex" alignItems="center" justifyContent="center">
+                Use Typescript and Material UI to create a React application, and then turn on prettier and eslint validation.
+            </Box>
+
+        </Container>
+    )
+}
+
+export default App
